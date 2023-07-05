00ab70436dcba81535138d9e22d15ba91660860f
Igor Nunes Amorim <43356551+roux3@users.noreply.github.com>
2023-07-05 13:18:53 -0300

    Estilizando Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,11 +1,31 @@
+//Styled-Componets
+import styled  from "styled-components"
+
 //Arquivos
 import banner from "../../../img/postaqui3.png"
 
 export default function Header(){
     return(
+        <StyledHeader>
         <div>
             <img src={banner}></img>
             <h1>Teste Calculadora Postaqui</h1>
         </div>
+        </StyledHeader>
     )
 }
+
+const StyledHeader = styled.div`
+    display: flex;
+    margin: 26px auto;
+    justify-content: center;
+    align-itens: center;
+    text-align: center;
+    
+    img{
+        width: 255px;
+        height: 74px;
+        flex-shrink: 0;
+    }
+
+`
